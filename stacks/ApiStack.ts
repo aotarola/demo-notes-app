9f9df6c9d0102b4f0f3a1e0b34ae16f986ec7ded
Andres Otarola <andres@otarola.me>
2023-05-21 16:52:36 -0700

    Adding a billing API

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -9,6 +9,9 @@ export function ApiStack({ stack, app }) {
       authorizer: 'iam',
       function: {
         bind: [table],
+        environment: {
+          STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY
+        }
       },
     },
     routes: {
@@ -16,7 +19,8 @@ export function ApiStack({ stack, app }) {
       'POST /notes': 'packages/functions/src/create.main',
       "GET /notes/{id}": 'packages/functions/src/get.main',
       "PUT /notes/{id}": 'packages/functions/src/update.main',
-      "DELETE /notes/{id}": 'packages/functions/src/delete.main'
+      "DELETE /notes/{id}": 'packages/functions/src/delete.main',
+      "POST /billing": 'packages/functions/src/billing.main',
     },
   });
 
